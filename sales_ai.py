f7ca71b54714470a000d669c6ba54145dfd4b095
kimkim2010 <libikimkim@gmail.com>
2026-01-28 21:19:17 +0100

    Update sales_ai.py

--- sales_ai.py
+++ sales_ai.py
@@ -35,7 +35,7 @@ Gmail: replyrindai@gmail.com
 def generate_reply(user_message):
 
     try:
-        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={GEMINI_API_KEY}"
+        url = f"https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
 
         payload = {
             "contents": [
